f1f3153fa5d0e9c0d5cf759e2aef7f1cadf81195
Jose-Miguel Pulido <josemiguelpulido@Jose-Miguels-MacBook-Pro.local>
2017-07-07 00:02:00 +0200

    initial version unscented kalman filter

---
+++ CarND-Unscented-Kalman-Filter-Project/src/tools.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "tools.h"
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using std::vector;
+
+Tools::Tools() {}
+
+Tools::~Tools() {}
+
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+                              const vector<VectorXd> &ground_truth) {
+  /**
+  TODO:
+    * Calculate the RMSE here.
+  */
+
+  VectorXd rmse(5);
+  rmse << 0,0,0,0,0;
+
+  // TODO: YOUR CODE HERE
+
+  // check the validity of the following inputs:
+  //  * the estimation vector size should not be zero
+  if (estimations.size() == 0) {
+    cout << "CalculateRMSE - estimation vector size zero" << endl;
+    return rmse;
+  }
+  //  * the estimation vector size should equal ground truth vector size
+  if (estimations.size() != ground_truth.size()) {
+    cout << "CalculateRMSE - estimation vector and ground truth vector have different sizes" << endl;
+    return rmse;
+  }
+
+  //accumulate squared residuals
+  VectorXd residuals(5);
+  residuals << 0,0,0,0,0;
+  for(int i=0; i < estimations.size(); ++i){
+    // ... your code here
+    VectorXd diff(5);
+    diff << estimations[i] - ground_truth[i];
+    VectorXd residual(5);
+    residual = diff.array()*diff.array();
+    residuals += residual;
+    
+  }
+
+  //calculate the mean
+  // ... your code here
+  residuals /= estimations.size();
+
+  //calculate the squared root
+  // ... your code here
+  rmse = residuals.array().sqrt();
+
+  //return the result
+  return rmse;
+
+}
